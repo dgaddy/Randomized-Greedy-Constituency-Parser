48f81bca65212ec44ceda4e642a920a653db8a22
David Gaddy <dgaddy@mit.edu>
2015-03-26 12:08:10 -0400

    Printed more stuff with CompareModels.

--- src/constituencyParser/CompareModels.java
+++ src/constituencyParser/CompareModels.java
@@ -81,12 +81,33 @@ public class CompareModels {
 			if (!(new HashSet<>(result)).equals(new HashSet<>(result2))) {
 				System.out.println("1 has " + commonWith1 + " correct");
 				System.out.println("2 has " + commonWith2 + " correct");
+				System.out.println(example.getWords());
+				SpanUtilities.printSpans(example.getSpans(), example.getWords().size(), labels);
 				
 				SpanUtilities.printSpans(result, example.getWords().size(), labels);
 				SpanUtilities.printSpans(result2, example.getWords().size(), labels);
 				
 				List<Long> features1 = Features.getAllFeatures(result, example.getWords(), true, words, labels, rules);
 				List<Long> features2 = Features.getAllFeatures(result2, example.getWords(), true, words, labels, rules);
+				
+				double secondOrderParamScoreForFirstOrder = 0;
+				double firstOrderParamScoreForFirstOrder = 0;
+				for(long f : features1) {
+					secondOrderParamScoreForFirstOrder += parameters2.getScore(f);
+					firstOrderParamScoreForFirstOrder += parameters.getScore(f);
+				}
+				
+				System.out.println("fo score for fo " + firstOrderParamScoreForFirstOrder + " so score for fo " + secondOrderParamScoreForFirstOrder);
+				
+				double secondOrderParamScoreForSecondOrder = 0;
+				double firstOrderParamScoreForSecondOrder = 0;
+				for(long f : features2) {
+					secondOrderParamScoreForSecondOrder += parameters2.getScore(f);
+					firstOrderParamScoreForSecondOrder += parameters.getScore(f);
+				}
+				
+				System.out.println("fo score for so " + firstOrderParamScoreForSecondOrder + " so score for so " + secondOrderParamScoreForSecondOrder);
+				
 				List<Long> common = new ArrayList<>(features1);
 				common.retainAll(features2);
 				System.out.println("model 1 only");
